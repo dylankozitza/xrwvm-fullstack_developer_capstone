17ffcad63cb9e823c68dd146a7bf403c171645cd
dylankozitza <112019138+dylankozitza@users.noreply.github.com>
2025-01-28 16:18:34 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -50,7 +50,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -59,7 +59,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -67,6 +67,7 @@ def registration(request):
     except Exception as e:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
+        logger.debug(f"Exception occurred: {e}")
 
     # If it is a new user
     if not username_exist:
@@ -146,12 +147,12 @@ def get_dealer_details(request, dealer_id):
 # def add_review(request):
 # ...
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
-        except:
+        except Exception as e:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
