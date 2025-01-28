dd4123ac4b3343dee99645922d9c66c6fa3cbb58
dylankozitza <112019138+dylankozitza@users.noreply.github.com>
2025-01-28 15:10:09 -0600

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -35,5 +35,5 @@ car_model_data = [
         # Add more CarModel instances as needed
 ]
 
-    for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+for data in car_model_data:
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
